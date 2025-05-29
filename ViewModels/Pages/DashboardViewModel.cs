b9ec34a280e5667fe408a793d7744941ae8af469
LimByungHun <165146080+LimByungHun@users.noreply.github.com>
2025-05-29 23:01:57 +0900

    Update DashboardViewModel.cs

--- ViewModels/Pages/DashboardViewModel.cs
+++ ViewModels/Pages/DashboardViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.ObjectModel;
+using System.Collections.ObjectModel;
 
 namespace 이거인가보오.ViewModels.Pages
 {
@@ -6,6 +6,9 @@ namespace 이거인가보오.ViewModels.Pages
     {
         [ObservableProperty]
         private int _counter = 0;
+
+        [ObservableProperty]
+        private double _accuracy = 0.0;
 
         [RelayCommand]
         private void OnCounterIncrement()
